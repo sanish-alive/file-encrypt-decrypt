c29c6d3cfc9248ad9afea2700a6d9aaa986a0329
mostanglan <xsonxhrestha2016@gmail.com>
2023-12-03 16:11:38 +0545

    decryption is implemented

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const upload = require('./upload')
-const encryptFile = require('./cryptograhp')
+const { encryptFile, decryptFile } = require('./cryptograhp')
 const hostname = "127.0.0.1"
 const port = 5000
 const app = express()
@@ -21,8 +21,32 @@ app.post('/', upload.single('myfile'), (req, res) => {
     const newFilename = req.file.filename
 
     encryptFile(newFilename)
+    res.send(`${newFilename}<a href="/">home<a><br><a href="/download/${newFilename}">download</a>`)
+    
+})
 
-    res.send(newFilename+'<a href="/">home<a>')
+app.get('/download/:filename', (req, res) => {
+    const filename = req.params.filename
+    const filePath = __dirname + '/encrypted-files/' + filename
+
+    res.download(filePath)
+})
+
+app.get('/decrypt', (req, res) => {
+    res.render('decrypt')
+})
+
+app.post('/decrypt', upload.single('myfile'), (req, res) => {
+    if(!req.file) {
+        return res.status(404).send('No file found')
+    }
+
+    const newFilename = req.file.filename
+
+    decryptFile(newFilename)
+
+    res.send(`decrypted file is done`)
+
 })
 
 app.listen(port, hostname, () => {
--- cryptograhp.js
+++ cryptograhp.js
@@ -13,16 +13,37 @@ const iv = crypto
     .substring(0, 16)
 
 const inputFilePath = 'uploads/'
-const outputFilePath = 'encrypted-files/'
+const encryptedFilePath = 'encrypted-files/'
+const decryptedFilePath = 'decrypted-files/'
 
 function encryptFile(fileName) {
     const inputBuffer = fs.readFileSync(inputFilePath+fileName)
 
     const cipher = crypto.createCipheriv('aes-256-cbc', secretKey, iv)
+    cipher.setAutoPadding(true)
     const encryptedBuffer = Buffer.concat([cipher.update(inputBuffer), cipher.final()]).toString('base64')
-    fs.writeFileSync(outputFilePath+fileName, encryptedBuffer)
+    fs.writeFileSync(encryptedFilePath + fileName, encryptedBuffer)
 
     console.log('File encrypted successfully')
 }
 
-module.exports = encryptFile
+function decryptFile(filename) {
+    const inputFile = inputFilePath+filename
+    if(!fs.existsSync(inputFile)){
+        console.error('Error: File not found!')
+    }
+    try {
+        const encryptedBuffer = Buffer.from(fs.readFileSync(inputFile, 'utf-8'), 'base64')
+        const decipher = crypto.createDecipheriv('aes-256-cbc', secretKey, iv)
+        decipher.setAutoPadding(true)
+        const decryptedBuffer = Buffer.concat([decipher.update(encryptedBuffer), decipher.final()])
+
+        fs.writeFileSync(decryptedFilePath + filename, decryptedBuffer)
+        
+        console.log('File decrypted successfully')
+    } catch (error) {
+        console.error("Error reading encrypted file: ", error.message)
+    }
+    
+}
+module.exports = { encryptFile, decryptFile }
